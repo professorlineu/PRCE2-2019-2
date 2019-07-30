4e421894d4c5411353fe47a101f5a5f4db944852
Professor Lineu Lima <53415085+professorlineu@users.noreply.github.com>
2019-07-29 22:04:06 -0300

    Envio das atividades de revisão

---
+++ Atividades/modelo.cpp
@@ -0,0 +1,25 @@
+/**********************************************************
+- Autor:     Seu nome
+- Descrição: Breve descrição do programa
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //Declaração de variáveis
+
+
+    //Configuração da tela de saída
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+    //Código do programa
+    cout << "Boa noite!";
+
+
+    return 0;
+}
