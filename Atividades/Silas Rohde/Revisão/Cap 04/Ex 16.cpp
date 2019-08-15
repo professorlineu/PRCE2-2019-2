f40c44f1b89ffb6b95e2cf5c42d06a69a582f633
sbrohded <53545042+sbrohded@users.noreply.github.com>
2019-08-14 22:22:22 -0300

    Add files via upload

---
+++ Atividades/Silas Rohde/Revisão/Cap 04/Ex 16.cpp
@@ -0,0 +1,45 @@
+/**********************************************************
+- Autor:     Silas Barboza Rohde
+- Descrição: Exercício 16 - Cap. 04
+**********************************************************/
+#include <iostream>
+#include <locale.h>
+#include <cstdlib>
+using namespace std;
+
+// protóripo da função
+float fPreco = 0;
+
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    system("color F1");
+
+
+cout << "Informe o preço do produto: ";
+cin >> fPreco;
+
+if (fPreco < 30 && fPreco > 0)
+{
+    cout << "\nProduto fora da promoção. Sem desconto!" << endl;
+}
+else if (fPreco > 30 && fPreco < 100)
+{
+    cout << "\nVocê terá 10% de desconto, o novo valor será de R$" << fPreco-((fPreco*10)/100) << endl;
+}
+else if (fPreco > 100)
+{
+    cout << "\nVocê terá 15% de desconto, o novo valor será de R$" << fPreco-((fPreco*10)/100) << endl;
+}
+else
+{
+    cout << "\nNúmero inválido!" << endl;
+}
+
+
+return 0;
+
+}
+
+
