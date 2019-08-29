73629d0e21dbfd8f88373423f2fac2991cf48af2
Felipe Alexandre <53545140+DarkShadow96-cpu@users.noreply.github.com>
2019-08-28 22:07:21 -0300

    Update lista2 atividade 4 a 9.cpp

--- Atividades/Felipe_Alexandre/revisão/lista2/lista2 atividade 4 a 9.cpp
+++ Atividades/Felipe_Alexandre/revisão/lista2/lista2 atividade 4 a 9.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
-
-#include <locale.h>
-
+#include <stdio.h>
 #include <cstdlib>
-
 #include <string>
-
 using namespace std;
-
 class Estoque
-
 {
-
 public:
+    void setcomeco()
+    {
+        printf("Interface de registro para estoque do produto.\nDigite os valores pedidos abaixo:\n");
+    }
 
     void setNome()
-
     {
 
         cin.ignore();
-
         cout<<"Informe o nome da pessoa: ";
-
         getline(cin,Nome);
-
     }
 
     void setValor()
@@ -31,123 +24,73 @@ public:
     {
 
         cout<<"Informe o valor do produto:R$";
-
         cin>>Valor;
-
         if(Valor<0)
 
         {
 
             cout<<"Valor invalido";
-
             exit(0);
-
         }
-
     }
+
+
 
     void setQuantidade()
-
     {
-
         cout<<"Informe a quantidade do produto disponivel: ";
-
         cin>>Quantidade;
-
         if(Quantidade<0)
-
         {
-
             cout<<"Valor invalido";
-
             exit(0);
-
         }
-
     }
-
     int setvalnum()
-
     {
-
         cout<<"Digite quantos produtos serão adicionados(quantidade máxima de 5 produtos): ";
-
-        cin>>valnum;
-
+     cin>>valnum;
         if(valnum<=0 ||valnum>5)
-
         {
-
-           cout<<"Valor invalido";
-
-           exit(0);
-
+            cout<<"Valor invalido";
+            exit(0);
         }
-
         return valnum;
-
     }
-
     void exibirDados()
-
     {
-
-        cout<<"\nDados do produto:\n";
-
+      cout<<"\nDados do produto:\n";
         cout<<"Nome: "<<Nome<<endl;
-
         cout<<"Quantidade: "<<Quantidade<<endl;
-
         cout<<"Valor: R$"<<Valor<<endl;
-
     }
-
 private:
-
-   string Nome;
-
-   int Quantidade;
-
-   float Valor;
-
-   int valnum;
-
+    string Nome;
+    int Quantidade;
+    float Valor;
+    int valnum;
 };
 
 int main()
 
 {
+    setlocale(LC_ALL,"");
+    Estoque e[5];
+    e[0].setcomeco();
+    int val=0;
+    int i=0;
+    val=e[i].setvalnum();
+    for(int i=0; i<val; i++)
+    {
+        e[i].setNome();
+        e[i].setValor();
+        e[i].setQuantidade();
+    }
 
-setlocale(LC_ALL,"");
+    for(int i=0; i<val; i++)
+    {
+        e[i].exibirDados();
+    }
 
-Estoque e[5];
-
-int val=0;
-
-int i=0;
-
-val=e[i].setvalnum();
-
-for(int i=0;i<val;i++)
-
-{
-
-  e[i].setNome();
-
-  e[i].setValor();
-
-  e[i].setQuantidade();
-
-}
-
-for(int i=0;i<val;i++)
-
-{
-
-  e[i].exibirDados();
-
-}
-
-return 0;
-
+    return 0;
 }
