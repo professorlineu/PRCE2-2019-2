f95e23904e8d5a601b97987472cd7b63e5bb1f9e
guilherme0170 <53454695+guilherme0170@users.noreply.github.com>
2019-09-24 11:16:30 -0300

    Update Ex19

--- Atividades/Guilherme_Marques/Revisao/Cap 04/Ex19.cpp
+++ Atividades/Guilherme_Marques/Revisao/Cap 04/Ex19.cpp
@@ -1,6 +1,6 @@
 /**********************************************************
 - Autor:     Guilherme Marques
-- Descriçãoo: Cálculo Peso Ideal
+- Descrição: Cálculo Peso Ideal
 **********************************************************/
 #include <iostream>
 #include <locale.h>
@@ -20,19 +20,19 @@ int main() {
   cin >> fAltura;
   cout << "\nQual o seu sexo (H/M): ";
   cin >> cResposta;
+  cResposta = toupper(cResposta);
 
-
-    if (cResposta == 'H' && fAltura > 0 && fAltura < 3)
+    if (isalpha(fAltura) || (cResposta == 'H' && fAltura > 0 && fAltura < 3))
     {
       cout << "\nO seu peso ideal é de " << (72*fAltura)-58 << " kilos";
     }
 
-    else if (cResposta == 'M' && fAltura > 0 && fAltura < 3)
+    else if (isalpha(fAltura) || (cResposta == 'M' && fAltura > 0 && fAltura < 3))
     {
       cout << "\nO seu peso ideal é de " << (62.1*fAltura)-44.7 << " kilos";
     }
     else
     {
-      cout << "\nResposta inválida!";
+      cout << "\nResposta inválida! Reinicie o Programa";
     }
 }
